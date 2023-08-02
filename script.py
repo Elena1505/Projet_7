030e24155eb41f3b6795f7577471aa1ac1821275
Elena1505 <eb.lelou@gmail.com>
2023-08-02 14:09:31 +0200

    Created function application_train (preprocess application_train.csv)

---
+++ script.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+
+# Preprocess application_train.csv
+def application_train(num_rows=None, nan_as_category=False):
+    # Read data
+    df = pd.read_csv('./application_train.csv', nrows=num_rows)
+    print("Train samples: {}".format(len(df)))
+    # Remove applications with XNA CODE_GENDER
+    df = df[df['CODE_GENDER'] != 'XNA']
+    # NaN values for DAYS_EMPLOYED: 365 243 -> nan
+    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace=True)
+    # Some simple new features (percentages)
+    df['DAYS_EMPLOYED_PERC'] = df['DAYS_EMPLOYED'] / df['DAYS_BIRTH']
+    df['INCOME_CREDIT_PERC'] = df['AMT_INCOME_TOTAL'] / df['AMT_CREDIT']
+    df['INCOME_PER_PERSON'] = df['AMT_INCOME_TOTAL'] / df['CNT_FAM_MEMBERS']
+    df['ANNUITY_INCOME_PERC'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL']
+    df['PAYMENT_RATE'] = df['AMT_ANNUITY'] / df['AMT_CREDIT']
+    return df
